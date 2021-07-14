6149044549b4cdf84b66ebaedb40bc0aaf00d0a6
DouglasSilvar <gogaplay@gmail.com>
2021-07-13 21:22:12 -0300

    update

--- backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
@@ -2,9 +2,9 @@ package com.devsuperior.dscatalog.services.exceptions;
 
 import com.sun.xml.bind.v2.schemagen.XmlSchemaGenerator;
 
-public class EntityNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException{
 
-    public EntityNotFoundException(String msg){
+    public ResourceNotFoundException(String msg){
         super(msg);
     }
 
