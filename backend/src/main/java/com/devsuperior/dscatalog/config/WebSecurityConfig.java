b00995b846e8134d1b31d86b32a0f0d3909f69c0
DouglasSilvar <gogaplay@gmail.com>
2022-04-24 22:04:46 -0300

    cors desable

--- backend/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
@@ -50,6 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().disable();
+        http.csrf().disable();
     }
 
     @Override
